e2567cd8e65309c6dc818e57c0ddcb6425d8e7f9
Jose <jose.lara.cuadrado@gmail.com>
2017-08-02 20:09:46 +0200

    reactive forms

--- src/app/componentes/model-driven/model-driven.component.ts
+++ src/app/componentes/model-driven/model-driven.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-model-driven',
@@ -6,7 +7,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModelDrivenComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  user: any = {
+    fullName: {
+      name: "Jose",
+      surname: "Lara"
+    },
+    email: "jose@lara.com",
+    hobbies:['run','eat','sleep']
+  }
+
+  constructor() {
+    this.form = new FormGroup({
+      'fullName': new FormGroup({
+
+        'name': new FormControl('', [Validators.required, Validators.minLength(3)]),
+        'surname': new FormControl('', [Validators.required,this.noShitValidation])
+      }),
+
+      'email': new FormControl('', [Validators.required,
+      Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]),
+      'hobbies': new FormArray([
+          new FormControl('run', Validators.required)
+      ])
+    });
+
+    // this.form.setValue(this.user);
+  }
+
+  addHobby(){
+    (<FormArray> this.form.controls['hobbies']).push(
+      new FormControl('', Validators.required)
+    )
+  }
+
+  //input invalid if the value is shit
+  noShitValidation(control: FormControl): {[s:string]: boolean}{
+    if(control.value === "shit"){
+      return{
+        noShitValidation: true
+      }
+    }
+  }
+
+  saveChanges() {
+    console.log(this.form.value);
+    console.log(this.form);
+    this.form.reset({
+      fullName: {
+        name: "",
+        surname: ""
+      },
+      email: ""
+    });
+    // this.form.controls['email'].setValue('');
+  }
 
   ngOnInit() {
   }
